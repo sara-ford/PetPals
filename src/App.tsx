f7e56be0539279dec09bf15246f0ebccd9c1b384
sara fordsham <sara@fordsham.com>
2025-06-09 20:33:23 +0300

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthContainer from './components/AuthContainer/AuthContainer';
 import Home from './components/Home/Home';
-
+import NavBar from './components/NavBar/NavBar';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <Router>
+      <NavBar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthContainer />} />
+        <Route path="/about" element={<div>דף מידע</div>} />
+      </Routes>
+    </Router>
   );
 };
 
