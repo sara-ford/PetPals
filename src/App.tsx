7b344e6834aa52222cf2c76b4499e936f172e736
sara fordsham <sara@fordsham.com>
2025-06-10 14:27:22 +0300

    aaaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AuthContainer from './components/AuthContainer/AuthContainer';
 import Home from './components/Home/Home';
-import NavBar from './components/NavBar/NavBar';
+
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <NavBar /> 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<AuthContainer />} />
-        <Route path="/about" element={<div>דף מידע</div>} />
-      </Routes>
-    </Router>
+    <div>
+      <AuthContainer />
+      <Home />
+    </div>
   );
 };
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,12 +5,13 @@ import './NavBar.scss';
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">🐶 PuppyCare</div>
+      <div className="navbar-logo">🐶 Petpals</div>
       <ul className="navbar-links">
         <li><Link to="/">התחברות</Link></li>
         <li><Link to="/home">דף הבית</Link></li>
-        <li><Link to="/about">אודות</Link></li>
-        <li><Link to="/contact">צור קשר</Link></li>
+        <li><Link to="/about">החיות שלנו</Link></li>
+        <li><Link to="/contact"> מועדפים</Link></li>
+        <li><Link to="/about"> הפרטים שלי</Link></li>
       </ul>
     </nav>
   );
