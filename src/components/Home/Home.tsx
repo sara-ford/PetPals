4935f5097112c7809a09ad1457cc25473d2d031c
sara fordsham <sara@fordsham.com>
2025-06-12 16:33:20 +0300

    שינוי

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,12 +7,6 @@ import { RootState } from '../../redux/store';
 
 
 const Home = ({ onShowPersonalInfo }: { onShowPersonalInfo: () => void }) => {
-  const [currentUser, setCurrentUser] = useState<any>(null);
-
-useEffect(() => {
-  const user = JSON.parse(localStorage.getItem('user') || '{}');
-  setCurrentUser(user);
-}, []);
   const [pets, setPets] = useState<any[]>([]);
   const [selectedPet, setSelectedPet] = useState<any>(null);
   const [statusFilter, setStatusFilter] = useState('');
